02e1ac5d323f97f2ded9852d941f93ed424768c9
Sam T <pxqr.sta@gmail.com>
2013-05-30 19:01:51 +0400

    ~ Use compact count format.

--- bench/Main.hs
+++ bench/Main.hs
@@ -47,26 +47,26 @@ fromByteString = S.fromDistinctAscList . indices 0 . B.unpack
 
 main :: IO ()
 main = defaultMain $
-  [ bench "fromList/O-2500"  $ nf S.fromList [0..2500]
-  , bench "fromList/O-5000"  $ nf S.fromList [0..5000]
-  , bench "fromList/O-10000" $ nf S.fromList [0..10000]
-  , bench "fromList/O-20000" $ nf S.fromList [0..20000]
-  , bench "fromList/O-20000" $ nf S.fromList (L.map (* 10) [0..20000])
-  , bench "fromList/S-2500"  $ nf SB.fromList [0..2500]
-  , bench "fromList/S-5000"  $ nf SB.fromList [0..5000]
-  , bench "fromList/S-10000" $ nf SB.fromList [0..10000]
-  , bench "fromList/S-20000" $ nf SB.fromList [0..20000]
-  , bench "fromList/S-20000-sparse" $ nf SB.fromList (L.map (* 10) [0..20000])
+  [ bench "fromList/O-2.5K"  $ nf S.fromList [0..2500]
+  , bench "fromList/O-5K"  $ nf S.fromList [0..5000]
+  , bench "fromList/O-10K" $ nf S.fromList [0..10000]
+  , bench "fromList/O-20K" $ nf S.fromList [0..20000]
+  , bench "fromList/O-20K" $ nf S.fromList (L.map (* 10) [0..20000])
+  , bench "fromList/S-2.5K"  $ nf SB.fromList [0..2500]
+  , bench "fromList/S-5K"  $ nf SB.fromList [0..5000]
+  , bench "fromList/S-10K" $ nf SB.fromList [0..10000]
+  , bench "fromList/S-20K" $ nf SB.fromList [0..20000]
+  , bench "fromList/S-20K-sparse" $ nf SB.fromList (L.map (* 10) [0..20000])
 
 
   , let !s = S.fromList [1..50000] in
-    bench "toList/50000" $ nf S.toList s
+    bench "toList/50K" $ nf S.toList s
 
   , let !s = SB.fromList [1..50000] in
-    bench "toList/50000" $ nf SB.toList s
+    bench "toList/50K" $ nf SB.toList s
 
   , let !bs = B.replicate 10000 255 in
-    bench "fromByteString/10000-O" $ whnf Main.fromByteString bs
+    bench "fromByteString/10K-O" $ whnf Main.fromByteString bs
 
   , let !bs = B.replicate 1048576 255 in
     bench "fromByteString/8M-S-dense" $ whnf SB.fromByteString bs
@@ -78,10 +78,10 @@ main = defaultMain $
     bench "fromByteString/8M-S-empty" $ whnf SB.fromByteString bs
 
   , let !s = S.fromList [0..1000000] in
-    bench "member/1000000" $ nf (L.all (`S.member` s)) [50000..100000]
+    bench "member/1M" $ nf (L.all (`S.member` s)) [50000..100000]
 
   , let !s = SB.fromList [0..1000000] in
-    bench "member/1000000" $ nf (L.all (`SB.member` s)) [50000..100000]
+    bench "member/1M" $ nf (L.all (`SB.member` s)) [50000..100000]
 
   , let !s = S.fromDistinctAscList [0,64..1000000 * 64 ] in
     bench "split/O-1M-10K-sparse" $
@@ -113,32 +113,32 @@ mergeTempl :: (S.IntSet  -> S.IntSet  -> S.IntSet)
            -> String -> [Benchmark]
 mergeTempl sop bop n =
   [ let (!a, !b) = (S.fromList [0,64..10000 * 64], S.fromList [1,65..10000 * 64]) in
-    bench (n ++"/O-10000-sparse-disjoint")  $ whnf (uncurry sop) (a, b)
+    bench (n ++"/O-10K-sparse-disjoint")  $ whnf (uncurry sop) (a, b)
 
   , let (!a, !b) = (S.fromList [0,64..10000 * 64], S.fromList [0,64..10000 * 64]) in
-    bench (n ++"/O-10000-sparse-overlap")  $ whnf (uncurry sop) (a, b)
+    bench (n ++"/O-10K-sparse-overlap")  $ whnf (uncurry sop) (a, b)
 
   , let (!a, !b) = (SB.fromList [0,64..10000 * 64], SB.fromList [1,65..10000 * 64]) in
-    bench (n ++ "/S-10000-sparse-disjoint") $ whnf (uncurry bop) (a, b)
+    bench (n ++ "/S-10K-sparse-disjoint") $ whnf (uncurry bop) (a, b)
 
   , let (!a, !b) = (SB.fromList [0,64..10000 * 64], SB.fromList [0,64..10000 * 64]) in
-    bench (n ++ "/S-10000-sparse-overlap") $ whnf (uncurry bop) (a, b)
+    bench (n ++ "/S-10K-sparse-overlap") $ whnf (uncurry bop) (a, b)
 
   , let (!a, !b) = (S.fromList [0,2..500000 * 2], S.fromList [1,3..500000 * 2]) in
-    bench (n ++ "/O-500000-dense-disjoint")  $ whnf (uncurry sop) (a, b)
+    bench (n ++ "/O-500K-dense-disjoint")  $ whnf (uncurry sop) (a, b)
 
   , let (!a, !b) = (S.fromList [0,2..500000 * 2], S.fromList [0,2..500000 * 2]) in
-    bench (n ++ "/O-500000-dense-overlap")  $ whnf (uncurry sop) (a, b)
+    bench (n ++ "/O-500K-dense-overlap")  $ whnf (uncurry sop) (a, b)
 
   , let (!a, !b) = (SB.fromList [0,2..500000 * 2], SB.fromList [1,3..500000 * 2]) in
-    bench (n ++ "/S-500000-dense-disjoint") $ whnf (uncurry bop) (a, b)
+    bench (n ++ "/S-500K-dense-disjoint") $ whnf (uncurry bop) (a, b)
 
   , let (!a, !b) = (SB.fromList [0,2..500000 * 2], SB.fromList [0,2..500000 * 2]) in
-    bench (n ++ "/S-500000-dense-overlap") $ whnf (uncurry bop) (a, b)
+    bench (n ++ "/S-500K-dense-overlap") $ whnf (uncurry bop) (a, b)
 
   , let (!a, !b) = (S.fromList [0..500000], S.fromList [0..500000]) in
-    bench (n ++ "/O-500000-buddy")  $ whnf (uncurry sop) (a, b)
+    bench (n ++ "/O-500K-buddy")  $ whnf (uncurry sop) (a, b)
 
   , let (!a, !b) = (SB.fromList [0..500000], SB.fromList [0..500000]) in
-    bench (n ++ "/S-500000-buddy") $ whnf (uncurry bop) (a, b)
+    bench (n ++ "/S-500K-buddy") $ whnf (uncurry bop) (a, b)
   ]
